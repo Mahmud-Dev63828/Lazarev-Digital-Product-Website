0f1829ebc570525a5655886510a2d94b3fa19882
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-03-29 14:34:04 +0600

    page3 completed

--- main.js
+++ main.js
@@ -38,9 +38,9 @@ function navAnimation() {
     });
   });
 }
-// ! page 2 animation
+// ! page2 animation
 function page2Animation() {
-  var rightElems = document.querySelectorAll(".right-elem");
+  const rightElems = document.querySelectorAll(".right-elem");
 
   rightElems.forEach(function (elem) {
     elem.addEventListener("mouseenter", function () {
@@ -57,12 +57,50 @@ function page2Animation() {
     });
     elem.addEventListener("mousemove", function (dets) {
       gsap.to(elem.childNodes[3], {
-        x: dets.x - elem.getBoundingClientRect().x - 90,
-        y: dets.y - elem.getBoundingClientRect().y - 215,
+        x:
+          dets.x -
+          elem.getBoundingClientRect().x -
+          90 /*getBoundingClientRect eta arektu bujte hobe */,
+        y: dets.y - elem.getBoundingClientRect().y - 80,
       });
     });
   });
 }
+// ! page3 animation
+function page3VideoAnimation() {
+  const page3Center = document.querySelector(".page3-center");
+  const video = document.querySelector("#page3 video");
 
+  page3Center.addEventListener("click", function () {
+    video.play();
+    gsap.to(video, {
+      transform: "scaleX(1) scaleY(1)",
+      opacity: 1,
+      borderRadius: 0,
+    });
+  });
+  video.addEventListener("click", function () {
+    video.pause();
+    gsap.to(video, {
+      transform: "scaleX(0.7) scaleY(0)",
+      opacity: 0,
+      borderRadius: "30px",
+    });
+  });
+
+  var sections = document.querySelectorAll(".sec-right");
+
+  sections.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      elem.childNodes[3].style.opacity = 1;
+      elem.childNodes[3].play();
+    });
+    elem.addEventListener("mouseleave", function () {
+      elem.childNodes[3].style.opacity = 0;
+      elem.childNodes[3].load();
+    });
+  });
+}
 navAnimation();
 page2Animation();
+page3VideoAnimation();
